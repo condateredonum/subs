0037d4f9503d51bf91b8e6a91af07cbca4df5802
gc1060031785118215 <gc1060031785118215@gmail.com>
2025-10-07 13:24:58 +0100

    Handling usernames

--- yt/scripts/scrape_yt.py
+++ yt/scripts/scrape_yt.py
@@ -8,28 +8,36 @@ from utils import save_to_md
 API_KEY = 'YOUTUBE_API_KEY'
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
+# Channel ID curl
+# curl "https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=2lazy2tryYT&key=API_KEY
+
+# Username curl
+# curl "https://youtube.googleapis.com/youtube/v3/channels?part=id&id=UCJjSDX-jUChzOEyok9XYRJQ&key=API_KEY
+
 def get_latest_videos(channel_id_or_username):
     """Fetch the latest 3 videos from the provided channel ID or username."""
     if "youtube.com/channel/" in channel_id_or_username:
-        channel_id = channel_id_or_username
+        # If it's a channel ID, use it directly.
+        channel_id = channel_id_or_username.split("/")[-1]  # Get the last part after "/channel/"
     elif "youtube.com/@" in channel_id_or_username:
         # For usernames, we need to fetch the channel ID first
-        username = channel_id_or_username.split('/')[-2]
-        request = youtube.channels().list(
-            part='id',
-            forUsername=username
+        username = channel_id_or_username.split('/')[-1]  # Extract the username correctly
+        request = youtube.search().list(  # Change to search instead of channels
+            part='snippet',
+            type='channel',  # Ensure we're searching for channels
+            q=username  # Search using the username
         )
         response = request.execute()
-
+        
         # Extract the channel ID
         if response['items']:
-            channel_id = response['items'][0]['id']
+            channel_id = response['items'][0]['id']['channelId']  # Get the channelId from snippet
         else:
             return []  # Return empty if no channel found
-
     else:
         return []  # Invalid input format
 
+    # Fetch the latest videos for the channel ID
     response = youtube.search().list(
         channelId=channel_id,
         part='id,snippet',
@@ -39,10 +47,12 @@ def get_latest_videos(channel_id_or_username):
 
     video_list = []
     for item in response['items']:
-        video_list.append({
-            'title': item['snippet']['title'],
-            'url': f"https://www.youtube.com/watch?v={item['id']['videoId']}"
-        })
+        # Ensure the item is of type "video" before accessing videoId
+        if item['id']['kind'] == 'youtube#video':
+            video_list.append({
+                'title': item['snippet']['title'],
+                'url': f"https://www.youtube.com/watch?v={item['id']['videoId']}"
+            })
     return video_list
 
 def scrape_videos(md_file_path):
