aa0f5a1b92082682996c7fd2ddbaeb515867f5e8
gc1060031785118215 <gc1060031785118215@gmail.com>
2025-10-07 13:30:27 +0100

    D'oh

--- yt/scripts/scrape_yt.py
+++ yt/scripts/scrape_yt.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 from channel_info import get_channel_ids, get_usernames
 from utils import save_to_md
 
-API_KEY = 'YOUTUBE_API_KEY'
+API_KEY = os.environ.get('YOUTUBE_API_KEY')
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 # Channel ID curl
