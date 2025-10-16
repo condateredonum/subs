d6245becd719806c945e2131c21564bf0ca9913b
gc1060031785118215 <gc1060031785118215@gmail.com>
2025-10-16 09:52:18 +0100

    Move video duration into function

--- yt/scripts/utils.py
+++ yt/scripts/utils.py
@@ -1,5 +1,10 @@
 # yt/scripts/utils.py
 from datetime import datetime
+from googleapiclient.discovery import build
+
+# https://console.cloud.google.com
+API_KEY = os.environ.get('YOUTUBE_API_KEY')
+youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 def save_to_md(videos, file_path='yt/latest.md'):
     """Save scraped video data to a Markdown file with a timestamp."""
@@ -19,3 +24,20 @@ def save_to_md(videos, file_path='yt/latest.md'):
             for video in video_list:
                 file.write(f"- [{video['title']}]({video['url']})\n")
             file.write("\n")  # Add a newline between channels
+
+def api_get_video_duration(video_id):
+    # https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=0pUlHrVNZqA&key=
+    video_content_request = youtube.videos().list(
+        part='contentDetails',
+        id=video_id
+    )
+    video_content_response = video_content_request.execute()
+    try:
+        if video_content_response['items']:
+            video_duration = video_content_response['items'][0]['contentDetails']['duration']
+        else:
+            video_duration = 'Live'
+    except (IndexError, KeyError):
+        video_duration = 'Duration-Error'
+    
+    return video_duration
